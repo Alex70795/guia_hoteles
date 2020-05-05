7ba4150a9c5d9c1a1b8c87f26c663cfa7264ebeb
Alex70795 <60244089+Alex70795@users.noreply.github.com>
2020-05-05 10:34:13 -0400

    first commit

---
+++ hello.js
@@ -0,0 +1,19 @@
+<html>
+<title>Par o Impar</title>
+<script>
+var valor=prompt("Introduce el numero para saber si es par o impar","");
+
+function parImpar(valor){
+alert("Entre a la funcion");
+if(valor%2==0){
+alert("El numero "+ valor + " Es par ");
+
+}else{
+alert("No es par");
+}
+
+}
+parImpar(valor);
+</script>
+
+</html>
